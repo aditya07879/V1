0d8839b351dab70505d8ed38c6939aaf1d92a1e2
aditya07879 <aditya07879@gmail.com>
2026-01-08 22:39:58 +0530

    ...

--- src/server.js
+++ src/server.js
@@ -40,5 +40,5 @@ app.use("/api/votes", require("./routes/vote.routes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
